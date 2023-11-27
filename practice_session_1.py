f7b026262b69429b19b63ebbad2ff19c07ca89b1
Muhammad Utoro <dioutoro@googlemail.com>
2023-11-27 14:18:49 +0100

    Practice for approximation function value

--- practice_session_1.py
+++ practice_session_1.py
@@ -1,11 +1,11 @@
 # in this practice session, the task is to approximate the derivative value of a function (sin(x)) using the
 # 2nd order approximation. In the lesson, it is shown that using the 1st order approximation resulting in 
 # inaccurate approximation compared to the exact value
+# My observation was that the approximation value is indeed closer to the exact value using the higher order and using the third- fourth-order 
+# approximation for the first derivative using central scheme will result in the same exact difference formule, hence same results
 
 import numpy as np
-import math
-import matplotlib
-from matplotlib import pyplot as mplot
+import matplotlib.pyplot as plt
 
 a = (np.pi/2)-0.35
 b = (np.pi/2)+0.15
@@ -14,33 +14,27 @@ fb = np.sin(b)
 ex_val = np.cos(a)
 deltaX = b-a
 frst_order = (fb - fa)/deltaX
-
 error = (np.abs((frst_order-ex_val)/ex_val))*100
+print('The first order value is: ', frst_order,'\nand the exact value is:', ex_val)
+print('The error is: ', error, '%')
 
 # now we implement the difference formula to estimate the first derivative
 # f'(a) = (-3*f(a) + 4*f(a+deltaX) - f(a+2*deltaX))/2*deltaX
 
+# This is a second-order approximation for the first derivative using forward scheme
 scnd_order = (4*(np.sin(a+deltaX)) - 3*(np.sin(a)) - np.sin(a+2*deltaX))/(2*deltaX)
 upd_error = (np.abs((scnd_order-ex_val)/ex_val))*100
+print('The second order value is: ', scnd_order,'\nand the exact value is:', ex_val)
+print('The error is: ', upd_error, '%') # we can see that the error decrease from 71% with 1st order approx. to about 0.58% with 2nd order
 
-print(scnd_order)
-print(upd_error) # we can see that the error decrease from 71% with 1st order approx. to about 0.58% with 2nd order
-
-# 3rd order aprroximation with central scheme
-thrd_order = (-1*(np.sin(a+2*deltaX)) + 8*(np.sin(a+deltaX)) - 8*(np.sin(a-deltaX)) + np.sin(a-2*deltaX))/(12*deltaX)
+# This is a third-order approximation for the first derivative using central scheme
+thrd_order = (-1*(np.sin(a+2*deltaX)) + 8*(np.sin(a+deltaX)) - 8*(np.sin(a-deltaX)) + np.sin(a-2*deltaX))/(12*deltaX) 
 upd_error = (np.abs((thrd_order-ex_val)/ex_val))*100
+print('The third order value is: ', thrd_order,'\nand the exact value is:', ex_val)
+print('The error is: ', upd_error, '%')
 
-print(thrd_order)
-print(upd_error)
-
-frth_order = (-3*(np.sin(a+4*deltaX)) + 16*(np.sin(a+3*deltaX)) - 36*(np.sin(a+2*deltaX)) + 48*(np.sin(a+deltaX)) - 25*(np.sin(a)))/(12*deltaX)
+# This is a fourth-order approximation for the first derivative using central scheme
+frth_order = (-1*(np.sin(a+2*deltaX)) + 8*(np.sin(a+deltaX)) - 8*(np.sin(a-deltaX)) + 1*(np.sin(a-2*deltaX)))/(12*deltaX)
 upd_error = (np.abs((frth_order-ex_val)/ex_val))*100
-print(upd_error)
-
-print(thrd_order)
-print(upd_error) 
-
-# However, it is observed that using higher order approximation (higher than 2nd order) do not produce less error 
-# percentage. For the 3rd order approx, the error is about 8% and 4th order approx produces the error about 1.6%
-# One more observation is that for the 2nd order approximation with central-scheme produces less error compared to
-# the forward-scheme. The error is about 0.2% compared to 0.58%.
+print('The fourth order value is: ', frth_order,'\nand the exact value is:', ex_val)
+print('The error is: ', upd_error, '%')
